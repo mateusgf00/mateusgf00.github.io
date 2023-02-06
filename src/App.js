636c8f56bac74240938beaba0ee508628a6c4fc4
mateusgf00 <mateusgf00@gmail.com>
2023-02-06 12:55:47 -0300

    removendo react router dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 
 import { ThemeProvider } from 'styled-components';
-import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyles from './assets/styles/global';
 import defaultTheme from './assets/styles/themes/default';
@@ -8,12 +7,10 @@ import Home from '../src/pages/home';
 
 function App() {
   return (
-    <BrowserRouter>
-      <ThemeProvider theme={defaultTheme}>
-        <GlobalStyles />
-        <Home />
-      </ThemeProvider>
-    </BrowserRouter>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
+      <Home />
+    </ThemeProvider>
   );
 }
 
